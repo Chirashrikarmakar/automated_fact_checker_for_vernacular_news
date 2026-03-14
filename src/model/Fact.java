c2bafac22cb4432dcb2bd323c975e3196565c726
Chirashri Karmakar <chirashrikarmakar345@gmail.com>
2026-03-14 19:11:21 +0530

    Create Fact.java

---
+++ src/model/Fact.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Fact {
+
+    private String factText;
+
+    // Constructor
+    public Fact(String factText) {
+        this.factText = factText;
+    }
+
+    // Getter
+    public String getFactText() {
+        return factText;
+    }
+
+    // Setter
+    public void setFactText(String factText) {
+        this.factText = factText;
+    }
+}
