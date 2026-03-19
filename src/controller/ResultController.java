a964560d68c9d0f909dc40c3617ce182a826bc9c
Chirashri Karmakar <chirashrikarmakar345@gmail.com>
2026-03-19 20:41:59 +0530

    commit

--- src/controller/ResultController.java
+++ src/controller/ResultController.java
@@ -3,6 +3,8 @@ package controller;
 public class ResultController {
 
     public void showResult(String result) {
-        System.out.println("Fact Check Result: " + result);
+        System.out.println("\n===== FACT CHECK RESULT =====");
+        System.out.println(result);
+        System.out.println("=============================");
     }
 }
--- src/service/factservice.java
+++ src/service/factservice.java
@@ -2,17 +2,63 @@ package service;
 
 import java.util.List;
 import model.Fact;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.*;
 
 public class FactService {
 
+    // 🔑 Paste your ScaleDown API key here
+    private static final String API_KEY = "dfok5YrHSg3HJp3TmJ8n7JJ8MxwrpQY6wX2Tx5Jc";
+
     public String checkFact(String news, List<Fact> facts) {
 
-        for (Fact fact : facts) {
-            if (fact.getFactText().equalsIgnoreCase(news)) {
-                return "Verified";
-            }
-        }
+        try {
+            URL url = new URL("https://api.scaledown.ai/v1/chat/completions");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        return "Needs Verification";
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Bearer " + API_KEY);
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setDoOutput(true);
+
+            String input = "{ \"model\": \"gpt-3.5-turbo\", \"messages\": [{\"role\": \"user\", \"content\": \"Check if this statement is true or false in one line: " + news + "\"}] }";
+
+            OutputStream os = conn.getOutputStream();
+            os.write(input.getBytes());
+            os.flush();
+            os.close();
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream())
+            );
+
+            StringBuilder response = new StringBuilder();
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                response.append(line);
+            }
+
+            br.close();
+
+            return "AI Result: " + response.toString();
+
+        } catch (Exception e) {
+
+            
+            for (Fact fact : facts) {
+
+                    String newsLower = news.toLowerCase();
+                    String factLower = fact.getFactText().toLowerCase();
+
+                    if (newsLower.contains(factLower)) {
+
+                        return "Likely True (Confidence: 90%)\nMatched Fact: " + fact.getFactText();
     }
 }
+            }
+
+           return "Needs Verification (Confidence: 40%)";
+        }
+    }
